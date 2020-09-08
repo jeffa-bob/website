0c34be58ea7fd3ad9eb8878f346ff21bcd0a2f5e
jeffa-bob <brody.tyler.46@gmail.com>
2020-09-07 17:15:40 -0700

    re added files

---
+++ pages.cs
@@ -0,0 +1,33 @@
+using System.Xml;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+
+namespace pdf2.web
+{
+    public class Page
+    {
+        private XmlDocument pagexml = new XmlDocument();
+        public string page;
+        public MarkupString pagemark { get { return new MarkupString(page); } }
+        public string urlAddress;
+
+
+        public Page(string? url, string @page = null)
+        {
+            if (url == null)
+            {
+                page = "#document\n<!DOCTYPE html>\n<html>\n<body>\nNOT VALID\n</body>\n</html>";
+            }
+
+            urlAddress = url;
+
+            if (@page != null)
+            {
+                this.page = @page;
+                pagexml.LoadXml(page);
+            }
+        }
+    }
+}
